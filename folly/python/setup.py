2f173a6917568735b7b28376be781c7fa1740ae5
Xiao Li <ilx@meta.com>
2025-02-01 17:01:15 -0800

    tmp

--- folly/python/setup.py
+++ folly/python/setup.py
@@ -18,23 +18,50 @@
 # Cython requires source files in a specific structure, the structure is
 # created as tree of links to the real source files.
 
+import os
+import sysconfig
+
 from Cython.Build import cythonize
 from Cython.Compiler import Options
 from setuptools import Extension, setup
 
-
 Options.fast_fail = True
+
+print("Building Cython extensions")
+cxx_flags = os.getenv("FOLLY_PYTHON_CXX_FLAGS", "")
+extra_compile_args = [f for f in cxx_flags.split(" ") if f]
+
+library_dirs = []
+include_dirs = [sysconfig.get_config_var("INCLUDEPY")]
+
+conda_home = os.getenv("CONDA_PREFIX")
+if conda_home:
+    include_dirs.append(conda_home + "/include")
+    library_dirs.append(conda_home + "/lib")
+
+libs = ["glog", "folly", "folly_python_cpp"]
+
+if os.getenv("CXX_SANITIZER"):
+    extra_compile_args.append("-fsanitize=address")
+    extra_compile_args.append("-fno-omit-frame-pointer")
+    libs.append("asan")
 
 exts = [
     Extension(
-        "folly.executor",
-        sources=["folly/executor.pyx"],
-        libraries=["folly", "glog", "double-conversion", "iberty"],
-    ),
-    Extension(
         "folly.iobuf",
         sources=["folly/iobuf.pyx"],
-        libraries=["folly", "glog", "double-conversion", "iberty"],
+        libraries=libs,
+        library_dirs=library_dirs,
+        include_dirs=include_dirs,
+        extra_compile_args=extra_compile_args,
+    ),
+    Extension(
+        "folly.executor",
+        sources=["folly/executor.pyx"],
+        libraries=libs,
+        library_dirs=library_dirs,
+        include_dirs=include_dirs,
+        extra_compile_args=extra_compile_args,
     ),
 ]
 
@@ -45,5 +72,8 @@ setup(
     package_data={"": ["*.pxd", "*.h"]},
     setup_requires=["cython"],
     zip_safe=False,
-    ext_modules=cythonize(exts, compiler_directives={"language_level": 3}),
+    ext_modules=cythonize(
+        exts,
+        compiler_directives={"language_level": 3},
+    ),
 )
